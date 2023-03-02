020a173a7cd4d4ebdd16adcb17c2f2ef3d59552e
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2023-03-02 19:33:21 +0400

    created LoginTest

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     * 
+     * @group apiv1Login
+     */
+    public function test_successfull_login(): void
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->postJson(route('api.v1.login'), [
+            'username' => $user->username,
+            'password' => 'password',
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json
+                    ->where('message', __('Successfully logged in'))
+                    ->has('token')
+                    ->where('user.username', $user->username)
+                    ->where("user.phone", $user->phone)
+                    ->where("user.email", $user->email)
+            );
+    }
+}
