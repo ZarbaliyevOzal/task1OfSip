fec9fbee1f85a579ca5ff3f164c8c594ca342b07
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2023-03-03 07:57:42 +0400

    fixed added registration_date field

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -21,6 +21,7 @@ class UserResource extends JsonResource
             'email_verified_at' => $this->email_verified_at,
             'username' => $this->username,
             'phone' => $this->phone,
+            'registration_date' => $this->registration_date,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
+            $table->timestamp('registration_date')->default(now());
             $table->rememberToken();
             $table->timestamps();
         });
