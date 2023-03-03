066965df7c6c420e85a2076e08ac7f127552ed1e
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2023-03-03 07:59:23 +0400

    changed route /login to /signin

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::name('api.v1.')->prefix('v1')->group(function() {
     Route::post('/signup', Signup::class);
-    Route::post('/login', Login::class)->name('login');
+    Route::post('/signin', Login::class)->name('login');
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/info', Profile::class)->name('info');
         Route::put('/info', UpdateInfo::class)->name('info.update');
