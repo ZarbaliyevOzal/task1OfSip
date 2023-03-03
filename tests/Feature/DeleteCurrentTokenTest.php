292974ff791563bc412c022aef2211fdaa3d0244
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2023-03-03 08:31:25 +0400

    created token delete test

---
+++ tests/Feature/DeleteCurrentTokenTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Traits\GenerateUserToken;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class DeleteCurrentTokenTest extends TestCase
+{
+    use RefreshDatabase,
+        GenerateUserToken;
+    
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $user = User::factory()->create();
+        // generate token
+        $token = $this->generateUserToken($user, []);
+        
+        $response = $this
+            ->withHeaders([
+                'Authorization' => "Bearer $token->plainTextToken",
+            ])
+            ->deleteJson(route('api.v1.token'), []);
+
+        $response->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('message', __('Token was successfully deleted'))
+                    ->has('token')
+                    ->etc()
+            );
+    }
+}
